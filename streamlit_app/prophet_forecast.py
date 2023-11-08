ca1edb001b09b6b33e5078dcd24286aa142c3317
TeeNguyenDA <thanhnguyen.ulaval@gmail.com>
2023-11-08 05:05:49 -0500

    modified:   streamlit_app/prophet_forecast.py modified:   streamlit_app/requirements.txt

--- streamlit_app/prophet_forecast.py
+++ streamlit_app/prophet_forecast.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from fbprophet import Prophet
 
 st.set_page_config(page_title='Prophet Forecast', page_icon="🔮")
 
